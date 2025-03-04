464158a21fb3f6895aadda93791581253cc229a4
Diana Montero <dianamontero38@gmail.com>
2025-03-04 14:08:27 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# module02_week05_day02
+
+# Arrays Part 2
+
+## Assignment: 
+
+In this assignment, you will practice using arrays in JavaScript to store data, and modifying arrays using shift, unshift, push, and pop.
+
+### Instructions
+
+#### Task 1: Setup Your Environment
+
+##### 1.1 New File
+
+- Open your preferred code editor (e.g., Visual Studio Code) and clone down your assignment repo.
+- Follow the instructions for the two exercises in the `arrays_lab_2.js` file.
+- If you need to test your code before submitting, use an online javascript editor such as codepen.
+- Submit the assignment once it is complete
+
+Congratulations! You've successfully completed the JavaScript Arrays Lab. Be sure to commit and push your changes back to Github to receive a grade.
+
---
+++ arrays_lab_2.js
@@ -0,0 +1,75 @@
+/**
+ * Excercise 1: Array Manipulation
+ */
+
+let colors  = ["Red", "Blue", "Green"]
+
+// Write code below use push to add "Yellow" to the end of the array.
+
+
+
+// Write code below use unshift to add "Purple" to the beginning of the array.
+
+
+
+// Write code below to use pop to remove the last item from the array.
+
+
+
+// Write code below to use shift to remove the first item from the array.
+
+
+
+// Print the final array to the console.
+console.log(colors)
+
+
+
+
+/**
+    Exercise 2: Sort Even and Odd Numbers
+    Objective: Combine array methods with logic, control flow, and looping.
+
+    Task:
+        - Write a for loop that iterates through the numbers array from the first index to the last index.
+
+            - Inside the loop, use an if-else statement to check if each number is even or odd.
+
+            - If the number is even, use the push method to add it to the evenNumbers array.
+
+            - If the number is odd, use the push method to add it to the oddNumbers array.
+
+            - After the number is added to either array, remove it from the original numbers array using shift.
+
+        - After the loop, Print the evenNumbers, oddNumbers, and numbers arrays to the console.
+
+        Things to consider as you write your loop:
+
+            - How do you programmatically determine if a number is Even or Odd?
+    
+
+    Success criteria: 
+    - After your for loop executes the evenNumbers array should contain all of the even numbers
+    - After your for loop executes the oddNumbers array should contain all of the odd numbers
+    - After your for loop executes the numbers array should now be empty.
+ */
+
+
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    let evenNumbers = []
+    let oddNumbers = []
+
+    // write your code below
+
+
+
+
+
+
+
+    // Print the three arrays to the console below, on separate lines
+
+
+
+
+
